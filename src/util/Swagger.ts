d5daa52cfeeab79054dd1fba9ad866851e8ae1e3
mezotv <mezotv@users.noreply.github.com>
2023-05-28 16:04:42 +0000

    style: format files

--- src/util/Swagger.ts
+++ src/util/Swagger.ts
@@ -1,38 +1,38 @@
 /* Import packages */
-import swaggerUI from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
-import { Express } from 'express';
-import { join } from 'path';
-import { readFileSync } from 'fs';
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import { Express } from "express";
+import { join } from "path";
+import { readFileSync } from "fs";
 
 export default (api: Express) => {
   const packageDataRaw = readFileSync(
-    join(__dirname, '..', '..', 'package.json')
+    join(__dirname, "..", "..", "package.json")
   );
   const packageData = JSON.parse(String(packageDataRaw));
 
   api.use(
-    '/docs',
+    "/docs",
     swaggerUI.serve,
     swaggerUI.setup(
       swaggerJsDoc({
         failOnErrors: true,
         definition: {
-          openapi: '3.0.0',
+          openapi: "3.0.0",
           info: {
-            title: 'Ghibli API',
-            version: packageData.version ? packageData.version : '0.0.0',
+            title: "Ghibli API",
+            version: packageData.version ? packageData.version : "0.0.0",
             description:
-              'The unofficial API for everything related to Ghibli Studios.',
+              "The unofficial API for everything related to Ghibli Studios.",
           },
         },
-        apis: [join(__dirname, '..', 'docs') + '/*.ts'],
+        apis: [join(__dirname, "..", "docs") + "/*.ts"],
       }),
       {
         explorer: false,
         customJs: `/assets/js/swagger.js`,
         customCssUrl: `/assets/css/swagger.css`,
-        customSiteTitle: 'Ghibli API',
+        customSiteTitle: "Ghibli API",
       }
     )
   );
