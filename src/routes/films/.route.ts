9b5e05353878541b5fb6de071388d1be2ea3ceed
Dominik K <dominik@koch-bautechnik.de>
2023-06-01 12:45:02 +0200

    fix: serachability and returns

--- src/routes/films/.route.ts
+++ src/routes/films/.route.ts
@@ -6,20 +6,22 @@ export default (urlName: string, data: any): Router => {
   const api = Router();
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  api.get(`/${urlName}:id`, (req: Request, res: Response) => {
+  api.get(`/${urlName.toLowerCase()}:id`, (req: Request, res: Response) => {
     let searchData = [];
+    const lowercaseId = req.params.id.toLowerCase();
+  
     if (req.params.id.split("-").length == 5) {
-      searchData = data.films.filter((f: any) => f.id == req.params.id);
+      searchData = data.films.filter((f: any) => f.id == lowercaseId);
     } else {
       searchData = data.films.filter((f: any) =>
-        f.title.includes(req.params.id)
+        f.title.toLowerCase().includes(lowercaseId)
       );
     }
-
+  
     if (searchData.length == 0) {
-      res.send("null");
+      res.status(404).json(null);
     } else {
-      res.json(searchData);
+      res.status(200).json(searchData);
     }
   });
 
