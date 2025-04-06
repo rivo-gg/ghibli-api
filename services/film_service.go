a57dfbf62ced2fd964922e9d5645ffce261a7490
Dominik Koch <dominik@koch-bautechnik.de>
2025-04-06 13:30:15 +0200

    draft: film service

---
+++ services/film_service.go
@@ -0,0 +1,9 @@
+package services
+
+func getFilmById(id string) string {
+	return "This is a film with ID: " + id
+}
+
+func LegacyFilmService() string {
+	return "This is the legacy film service"
+}
